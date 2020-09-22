afdd766a7f3c43d665392e243b4f1321209c76cf
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-09-21 22:34:15 -0400

    solution pt 1

---
+++ src/User/UserInput.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserInput = () => {
+  return <input />;
+};
+
+export default UserInput;
---
+++ src/User/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = (props) => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Paragraph 2</p>
+    </div>
+  );
+};
+
+export default UserOutput;
